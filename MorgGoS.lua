ff037edb8b3cbb60f3dc533abee9949c0e2043e1
TrooperHDxLeagueSharp <jannikmarxx@gmail.com>
2017-01-14 18:35:02 +0100

    morg

--- MorgGoS.lua
+++ MorgGoS.lua
@@ -12,41 +12,41 @@ require("OpenPredict")
 
 require("DamageLib")
 
-local MorganaGoS = Menu("Morgana", "Morgana")
-MorganaGoS("Combo", "Combo")
-MorganaGoS:Boolean("QComb", "Use Q", true)
-MorganaGoS:Boolean("WCom", "Use W", true)
-MorganaGoS:Boolean("RComb", "Use R", true)
-MorganaGoS:Slider("MinMana", "Min Mana To Combo",50,0,100,1)
+local TroopMorg = Menu("Morgana", "Morgana")
+TroopMorg("Combo", "Combo")
+TroopMorg:Boolean("QComb", "Use Q", true)
+TroopMorg:Boolean("WCom", "Use W", true)
+TroopMorg:Boolean("RComb", "Use R", true)
+TroopMorg:Slider("MinMana", "Min Mana To Combo",50,0,100,1)
 
-MorganaGoS:SubMenu("Harass", "Harass", true)
-MorganaGoS.Harass:Boolean("WHarass", "Use W", true)
-MorganaGoS.Harass:Slider("MinManaHarass", "Min Mana To Harass",50,0,100,1)
+TroopMorg:SubMenu("Harass", "Harass", true)
+TroopMorg.Harass:Boolean("WHarass", "Use W", true)
+TroopMorg.Harass:Slider("MinManaHarass", "Min Mana To Harass",50,0,100,1)
 
-MorganaGoS:SubMenu("LaneClear", "LaneClear", true)
-MorganaGoS.LaneClear:Boolean("Qlc", "Use Q", true)
-MorganaGoS.LaneClear:Boolean("Wlc", "Use W", true)
-MorganaGoS.LaneClear:Slider("WMin", "Min Minions To R",9,1,15,1)
-MorganaGoS.LaneClear:Slider("MinManaLC", "Min Mana To LaneClear",50,0,100,1)
+TroopMorg:SubMenu("LaneClear", "LaneClear", true)
+TroopMorg.LaneClear:Boolean("Qlc", "Use Q", true)
+TroopMorg.LaneClear:Boolean("Wlc", "Use W", true)
+TroopMorg.LaneClear:Slider("WMin", "Min Minions To R",9,1,15,1)
+TroopMorg.LaneClear:Slider("MinManaLC", "Min Mana To LaneClear",50,0,100,1)
 
-MorganaGoS:SubMenu("Misc", "Misc")
-MorganaGoS:Boolean("UltX", "Auto R on X Enemies", true)
-MorganaGoS.Misc:Slider("EnemieR", "Min Enemies to Auto R",3,1,6,1)
-MorganaGoS.Misc:Boolean("Lvlup", "Use Auto Level", true)
-MorganaGoS.Misc:Boolean("Ig", "Use Auto Ignite", true)
-MorganaGoS.Misc:Boolean("Ebs", "Use Blackshield", true)
+TroopMorg:SubMenu("Misc", "Misc")
+TroopMorg:Boolean("UltX", "Auto R on X Enemies", true)
+TroopMorg.Misc:Slider("EnemieR", "Min Enemies to Auto R",3,1,6,1)
+TroopMorg.Misc:Boolean("Lvlup", "Use Auto Level", true)
+TroopMorg.Misc:Boolean("Ig", "Use Auto Ignite", true)
+TroopMorg.Misc:Boolean("Ebs", "Use Blackshield", true)
 
-MorganaGoS:SubMenu("SkinChanger", "SkinChanger")
+TroopMorg:SubMenu("SkinChanger", "SkinChanger")
 
 local skinMeta = {["Morgana"] = {"Classic", "Blade-Mistress", "Blackthorn", "Ghost-Bridge", "Exiled", "Sinful-Succulence", "Lumar-Wraith", "Bewitchintg", "Victorious"}}
-MorganaGoS.SkinChanger:DropDown('skin', myHero.charName.. " Skins", 1, skinMeta[myHero.charName], HeroSkinChanger, true)
-MorganaGoS.SkinChanger.skin.callback = function(model) HeroSkinChanger(myHero, model - 1) print(skinMeta[myHero.charName][model] .." ".. myHero.charName .. " Loaded!") end
+TroopMorg.SkinChanger:DropDown('skin', myHero.charName.. " Skins", 1, skinMeta[myHero.charName], HeroSkinChanger, true)
+TroopMorg.SkinChanger.skin.callback = function(model) HeroSkinChanger(myHero, model - 1) print(skinMeta[myHero.charName][model] .." ".. myHero.charName .. " Loaded!") end
 
-MorganaGoS:SubMenu("Draw", "Drawings")
-MorganaGoS.Draw:Boolean("DrawQ", "Draw Q Range", true)
-MorganaGoS.Draw:Boolean("DrawW", "Draw W Range", true)
-MorganaGoS.Draw:Boolean("DrawE", "Draw E Range", true)
-MorganaGoS.Draw:Boolean("DrawR", "Draw R Range", true)
+TroopMorg:SubMenu("Draw", "Drawings")
+TroopMorg.Draw:Boolean("DrawQ", "Draw Q Range", true)
+TroopMorg.Draw:Boolean("DrawW", "Draw W Range", true)
+TroopMorg.Draw:Boolean("DrawE", "Draw E Range", true)
+TroopMorg.Draw:Boolean("DrawR", "Draw R Range", true)
 
 OnTick(function ()
 	
@@ -55,7 +55,7 @@ OnTick(function ()
 	local GetPercentMana = (GetCurrentMana(myHero) / GetMaxMana(myHero)) * 100
 	local target = GetCurrentTarget()
 	
-	if MorganaGoS.Misc.Lvlup:Value() then
+	if TroopMorg.Misc.Lvlup:Value() then
 		spellorder = {_Q, _W, _E, _Q, _Q, _R, _Q, _W, _Q, _W, _R, _W, _W, _E, _E, _R, _E, _E}	
 		if GetLevelPoints(myHero) > 0 then
 			LevelSpell(spellorder[GetLevel(myHero) + 1 - GetLevelPoints(myHero)])
@@ -64,21 +64,21 @@ OnTick(function ()
 
 	if Mode() == "Combo" then
 		
-		if MorganaGoS.Combo.QComb:Value() and Ready(_Q) and ValidTarget(target, 1175) then
-				if MorganaGoS.Combo.MinMana:Value() <= GetPercentMana then 
+		if TroopMorg.Combo.QComb:Value() and Ready(_Q) and ValidTarget(target, 1175) then
+				if TroopMorg.Combo.MinMana:Value() <= GetPercentMana then 
 					CastSkillShot(target, _Q)	
 				end
 			end
 		
 
-		if MorganaGoS.Combo.WComb:Value() and Ready(_W) and ValidTarget(target, 900) then
-			if MorganaGoS.Combo.MinMana:Value() <= GetPercentMana then
+		if TroopMorg.Combo.WComb:Value() and Ready(_W) and ValidTarget(target, 900) then
+			if TroopMorg.Combo.MinMana:Value() <= GetPercentMana then
 					CastTargetSpell(_W)	
 			end
 		end
 	
-		if MorganaGoS.Combo.RComb:Value() and Ready(_R) and ValidTarget(target, 400) then
-				if MorganaGoS.Combo.MinMana:Value() <= GetPercentMana then 
+		if TroopMorg.Combo.RComb:Value() and Ready(_R) and ValidTarget(target, 400) then
+				if TroopMorg.Combo.MinMana:Value() <= GetPercentMana then 
 					CastSpell(target, _R)	
 			end
 		end
@@ -87,8 +87,8 @@ OnTick(function ()
 	
 	if Mode() == "Harass" then
 		
-		if MorganaGoS.Harass.WHarass:Value() and Ready(_W) and ValidTarget(target, 900) then
-			if MorganaGoS.Harass.MinManaHarass:Value() <= GetPercentMana then
+		if TroopMorg.Harass.WHarass:Value() and Ready(_W) and ValidTarget(target, 900) then
+			if TroopMorg.Harass.MinManaHarass:Value() <= GetPercentMana then
 				CastTargetSpell(target, _W)
 			end
 		end
@@ -98,15 +98,15 @@ OnTick(function ()
 if Mode() == "LaneClear" then
 		
 		for _, closeminion in pairs(minionManager.objects) do
-			if MorganaGoS.LaneClear.Qlc:Value() and ValidTarget(closeminion, 1125) then
-				if GetPercentMP(myHero) >= MorganaGoS.LaneClear.MinManaLC:Value() then
+			if TroopMorg.LaneClear.Qlc:Value() and ValidTarget(closeminion, 1125) then
+				if GetPercentMP(myHero) >= TroopMorg.LaneClear.MinManaLC:Value() then
 					CastSkillShot(_Q, closeminion)
 				end
 			end
 			
 
-			if MorganaGoS.LaneClear.Wlc:Value() and ValidTarget(closeminion, 900) >= MorganaGoS.LaneClear.WMin:Value() then
-				if GetPercentMP(myHero) >= MorganaGoS.LaneClear.MinManaLC:Value() then
+			if TroopMorg.LaneClear.Wlc:Value() and ValidTarget(closeminion, 900) >= TroopMorg.LaneClear.WMin:Value() then
+				if GetPercentMP(myHero) >= TroopMorg.LaneClear.MinManaLC:Value() then
 					CastSkillShot(_W, closeminion)
 				end
 			end
@@ -117,12 +117,12 @@ end
 
 	for _, enemy in pairs(GetEnemyHeroes()) do
 			--AutoR
-		if MorganaGoS.Misc.UltX:Value() and Ready(_R) and ValidTarget(enemy, 1000) and EnemiesAround(enemy, 300) >= MorganaGoS.Misc.EnemirR:Value() then
+		if TroopMorg.Misc.UltX:Value() and Ready(_R) and ValidTarget(enemy, 1000) and EnemiesAround(enemy, 300) >= TroopMorg.Misc.EnemirR:Value() then
 				CastTargetSpell(enemy, _R)
 			end	
 			--Auto Ignite 
 		if GetCastName(myHero, SUMMONER_1):lower():find("summonerdot") then
-			if MorganaGoS.Misc.Ig:Value() and Ready(SUMMONER_1) and ValidTarget(enemy, 600) then
+			if TroopMorg.Misc.Ig:Value() and Ready(SUMMONER_1) and ValidTarget(enemy, 600) then
 				if GetCurrentHP(enemy) < IgDamage then
 					CastTargetSpell(enemy, SUMMONER_1)
 				end
@@ -130,7 +130,7 @@ end
 		end
 	
 		if GetCastName(myHero, SUMMONER_2):lower():find("summonerdot") then
-			if MorganaGoS.Misc.Ig:Value() and Ready(SUMMONER_2) and ValidTarget(enemy, 600) then
+			if TroopMorg.Misc.Ig:Value() and Ready(SUMMONER_2) and ValidTarget(enemy, 600) then
 				if GetCurrentHP(enemy) < IDamage then
 					CastTargetSpell(enemy, SUMMONER_2)
 				end
@@ -144,10 +144,10 @@ end)
 OnDraw(function(myHero)
 	local pos = GetOrigin(myHero)
 	local mpos = GetMousePos()
-	if MorganaGoS.Draw.DrawQ:Value() then DrawCircle(pos, 1125, 1, 25, GoS.Red) end
-	if MorganaGoS.Draw.DrawW:Value() then DrawCircle(pos, 900, 1, 25, GoS.Blue) end
-	if MorganaGoS.Draw.DrawE:Value() then DrawCircle(pos, 600, 1, 25, GoS.Blue) end
-	if MorganaGoS.Draw.DrawR:Value() then DrawCircle(pos, 600, 1, 25, GoS.Green) end
+	if TroopMorg.Draw.DrawQ:Value() then DrawCircle(pos, 1125, 1, 25, GoS.Red) end
+	if TroopMorg.Draw.DrawW:Value() then DrawCircle(pos, 900, 1, 25, GoS.Blue) end
+	if TroopMorg.Draw.DrawE:Value() then DrawCircle(pos, 600, 1, 25, GoS.Blue) end
+	if TroopMorg.Draw.DrawR:Value() then DrawCircle(pos, 600, 1, 25, GoS.Green) end
 end)	
 
 
